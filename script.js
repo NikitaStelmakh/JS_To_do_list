ce55923f0709cd14dd7e95ac47facc57048d4191
NikitaStelmakh <nikita.stelmakh98@gmail.com>
2026-06-08 10:34:03 +0200

    counter function managed

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const updateCounters = () => {
+   const checkboxes = document.querySelectorAll(".check_box");
+
+   const completedCount = [...checkboxes].filter(cb => cb.checked).length;
+
+   const inProgressCount = checkboxes.length - completedCount;
+
+   document.getElementById("completed").textContent =`выполненные: ${completedCount}`;
+
+   document.getElementById("in_progress").textContent = `невыполненные: ${inProgressCount}`;
+
+};
+
+
 document.getElementById("task_form").addEventListener("submit", function (evt) {
 evt.preventDefault();
 
@@ -5,7 +19,27 @@ const taskInput = document.getElementById("task");
 const value = taskInput.value;
 const addBtn = document.getElementById("add_btn");
 
-document.getElementById("task_list").innerHTML +=`<li>${value}</li>`;
+const li = document.createElement("li");
+const checkbox = document.createElement("input");
+checkbox.type ="checkbox";
+checkbox.className = "check_box";
+li.appendChild(checkbox);
+li.append(`${value}`);
+
+document.getElementById("task_list").appendChild(li);
+
+updateCounters ();
+
 
 taskInput.value = "";
+
+
 });
+
+
+document.getElementById("task_list").addEventListener("change", function(evt) {
+if(evt.target.type === "checkbox") {
+  updateCounters ();  
+}
+});
+

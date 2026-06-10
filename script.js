e40061e5f4181d53630bfb44eba8ef0b1f548229
NikitaStelmakh <nikita.stelmakh98@gmail.com>
2026-06-10 12:53:38 +0200

    counters logic + css style

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const skippedElement = document.getElementById("skipped");
+let skippedCount = 0;
+
 const updateCounters = () => {
    const checkboxes = document.querySelectorAll(".check_box");
 
@@ -13,33 +16,51 @@ const updateCounters = () => {
 
 
 document.getElementById("task_form").addEventListener("submit", function (evt) {
-evt.preventDefault();
+  evt.preventDefault();
 
-const taskInput = document.getElementById("task");
-const value = taskInput.value;
-const addBtn = document.getElementById("add_btn");
+   const taskInput = document.getElementById("task");
+   const value = taskInput.value;
+   const addBtn = document.getElementById("add_btn");
 
-const li = document.createElement("li");
-const checkbox = document.createElement("input");
-checkbox.type ="checkbox";
-checkbox.className = "check_box";
-li.appendChild(checkbox);
-li.append(`${value}`);
+   const li = document.createElement("li");
+   const checkbox = document.createElement("input");
+   checkbox.type ="checkbox";
+   checkbox.className = "check_box";
 
-document.getElementById("task_list").appendChild(li);
+   const deleteBtn = document.createElement("input");
+   deleteBtn.type = "button";
+   deleteBtn.className ="delete_btn";
+   deleteBtn.value = "❌";
 
-updateCounters ();
+   li.appendChild(checkbox);
+   li.append(`${value}`);
+   li.appendChild(deleteBtn);
+
+   document.getElementById("task_list").appendChild(li);
+
+   updateCounters ();
 
 
-taskInput.value = "";
+   taskInput.value = "";
+  
+   const skippedCounter = () => {
+    if(!checkbox.checked) {
+      skippedCount++;
+      skippedElement.textContent = `отклоненные: ${skippedCount}`;
+      }
+  };
 
-
+   deleteBtn.addEventListener("click", () => {
+     skippedCounter();
+     li.remove();
+     updateCounters();
+});
 });
 
 
 document.getElementById("task_list").addEventListener("change", function(evt) {
-if(evt.target.type === "checkbox") {
+   if(evt.target.type === "checkbox") {
   updateCounters ();  
-}
+   }
 });
 
